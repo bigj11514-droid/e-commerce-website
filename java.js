f3bd7386e4f1a41758321ffe88a965ca7cb0dedb
Nartey <bigj11514@gmail.com>
2026-07-20 22:29:04 +0000

    e commerce website

---
+++ java.js
@@ -0,0 +1,8 @@
+console.log("Yng-Archi  Official Bot");
+
+const botName = "Yng Bot";
+const botLocation = "Ghana";
+const botIntro = `I am ${botName}. I am going to be your PA Assistant to help you navigate this website. I am operation from ${botLocation}.`;
+ console.log(botIntro);
+console.log(`Please feel free to ask any question`);
+
